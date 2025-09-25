a7550ebb9943f9b895a5473da69c6eb03e1d9a9a
Narine Kazaryan <kaznarine5@gmail.com>
2025-09-25 12:30:03 +0300

    Удалил webDriver/bin и обнавил generators.py

--- tests/generators.py
+++ tests/generators.py
@@ -1,42 +1,20 @@
 import random
 import string
-import pytest
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import  expected_conditions as EC
-from locators import *
+
 
 URL = "https://stellarburgers.nomoreparties.site/"
-def generate_email(name="narine"):
-    cohort = "31"
-    digits = random.randint(100, 999)
-    return f"{name}_kazaryan_{cohort}_{digits}@ya.ru"
+def generate_email():
+    return f"Narine_kazaryan_31_{random.randint(100,999)}@yandex.ru"
+
 def generate_password(length=6):
-    chars = string.ascii_letters + string.digits
-    return ''.join(random.choice(chars) for _ in range(length))
+    return "".join(random.choices(string.ascii_letters + string.digits, k=length))
 
+def generate_name():
+    return "Test User"
+print(generate_email())
+print(generate_password())
+print(generate_name())
 
-def login(driver, email, password):
-    driver.get(URL)
-    WebDriverWait(driver, 10).until(EC.visibility_of_element_located(LOGIN_EMAIL_INPUT)).send_keys(email)
-    driver.find_element(*LOGIN_PASSWORD_INPUT).send_keys(password)
-    driver.find_element(*LOGIN_BUTTON).click()
-    WebDriverWait(driver, 10).until(EC.visibility_of_element_located(ACCOUNT_HEADER))
-
-def test_registration_and_login(driver):
-    email = generate_email("Narine")
-    password = generate_password(6)
-
-    driver.get(URL + "register")
-    driver.find_element(*NAME_INPUT).send_keys("Narine")
-    driver.find_element(*EMAIL_INPUT).send_keys(email)
-    driver.find_element(*PASSWORD_INPUT).send_keys(password)
-    driver.find_element(*REGISTER_BUTTON).click()
-
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located(LOGIN_BUTTON))
-    assert driver.find_element(*LOGIN_BUTTON).is_displayed()
-
-    login(driver, email, password)
-    assert driver.find_element(*ACCOUNT_HEADER).is_displayed()
 
 
 
