9a5eb41f84dfe5a6a526ad9701e7cfb68502bd50
Narine Kazaryan <kaznarine5@gmail.com>
2025-09-26 21:30:55 +0300

    Добавлены тесты и README.md для develop

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from tests.generators import generate_email, generate_password, generate_name
+
 CHROMEDRIVER_PATH = "C:\\Users\\Artar\\PycharmProjects\\Sprint_5\\WebDriver\\bin\\chromedriver.exe"
 
 
@@ -10,3 +12,10 @@ def driver():
     driver.maximize_window()
     yield driver
     driver.quit()
+@pytest.fixture
+def user_data():
+    return {
+        "email": generate_email(),
+        "password": generate_password(),
+        "name": generate_name()
+             }
