9ee8392e1fcf017e436f2b2da2217dc2e5ab020e
Narine Kazaryan <kaznarine5@gmail.com>
2025-10-01 09:13:29 +0300

    fix: обновил  tests/test_registration.py tests/test_constructor.py

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -5,89 +5,89 @@ from config import BASE_URL
 import pytest
 from helpers import  login
 
-
+class TestConstructor:
 
 
 # Кнопка "Войти в аккаунт" на главной
-def test_go_to_account(driver):
-    driver.get(BASE_URL)
-    driver.find_element(*ACCOUNT_HEADER).click()
-    WebDriverWait(driver, 10).until(EC.visibility_of_element_located(LOGIN_BUTTON))
-    assert driver.find_element(*LOGIN_BUTTON).is_displayed()
+    def test_go_to_account(driver):
+        driver.get(BASE_URL)
+        driver.find_element(*ACCOUNT_HEADER).click()
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located(LOGIN_BUTTON))
+        assert driver.find_element(*LOGIN_BUTTON).is_displayed()
 
 # Кнопка "Личный кабинет"
-def test_personal_account(driver):
-    driver.get(BASE_URL)
-    driver.find_element(*ACCOUNT_HEADER).click()
-    WebDriverWait(driver, 10).until(EC.visibility_of_element_located(LOGIN_BUTTON))
-    assert driver.find_element(*ACCOUNT_HEADER).is_displayed()
+    def test_personal_account(driver):
+        driver.get(BASE_URL)
+        driver.find_element(*ACCOUNT_HEADER).click()
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located(LOGIN_BUTTON))
+        assert driver.find_element(*ACCOUNT_HEADER).is_displayed()
 
 
 
 # Кнопка в форме регистрации
-def test_registretion_account(driver,user_data):
-    driver.get(BASE_URL)
-    driver.find_element(*REGISTER_BUTTON).click()
-    driver.find_element(*NAME_INPUT).send_keys(user_data["name"])
-    driver.find_element(*EMAIL_INPUT).send_keys(user_data["email"])
-    driver.find_element(*PASSWORD_INPUT).send_keys(user_data["password"])
-    WebDriverWait(driver, 10).until((EC.visibility_of_element_located(ACCOUNT_HEADER)))
-    assert driver.find_element(*ACCOUNT_HEADER).is_displayed()
+    def test_registretion_account(driver,user_data):
+        driver.get(BASE_URL)
+        driver.find_element(*REGISTER_BUTTON).click()
+        driver.find_element(*NAME_INPUT).send_keys(user_data["name"])
+        driver.find_element(*EMAIL_INPUT).send_keys(user_data["email"])
+        driver.find_element(*PASSWORD_INPUT).send_keys(user_data["password"])
+        WebDriverWait(driver, 10).until((EC.visibility_of_element_located(ACCOUNT_HEADER)))
+        assert driver.find_element(*ACCOUNT_HEADER).is_displayed()
 
 
 
 #Восстановления пароля
-def test_login_from_recovery_from(driver, user_data):
-    driver.get(BASE_URL + "forgot-password")
-    driver.find_element(By.XPATH, '//*[text()="Войти"]').click()
-    login(driver, user_data["email"], user_data["password"])
-    assert driver.find_element(*ACCOUNT_HEADER).is_displayed()
+    def test_login_from_recovery_from(driver, user_data):
+        driver.get(BASE_URL + "forgot-password")
+        driver.find_element(By.XPATH, '//*[text()="Войти"]').click()
+        login(driver, user_data["email"], user_data["password"])
+        assert driver.find_element(*ACCOUNT_HEADER).is_displayed()
 
 # Переход в личный кабинет
-def  test_login(driver, email, password):
-    WebDriverWait(driver, 10).until(EC.visibility_of_element_located(LOGIN_EMAIL_INPUT)).send_keys(email)
-    driver.find_element(*LOGIN_PASSWORD_INPUT).send_keys(password)
-    driver.find_element(*LOGIN_BUTTON).click()
-    WebDriverWait(driver, 10).until(EC.visibility_of_element_located(ACCOUNT_HEADER))
+    def test_login(driver, email, password):
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located(LOGIN_EMAIL_INPUT)).send_keys(email)
+        driver.find_element(*LOGIN_PASSWORD_INPUT).send_keys(password)
+        driver.find_element(*LOGIN_BUTTON).click()
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located(ACCOUNT_HEADER))
 
 
 
 
 #Переход из личного кабинета в конструктор
-def test_go_to_constructor_from_logo(driver, user_data):
-     driver.get(BASE_URL)
-     login(driver, user_data["email"], user_data["password"])
-     WebDriverWait(driver,10).until(EC.visibility_of_element_located(ACCOUNT_HEADER)).click()
-     WebDriverWait(driver,10).until(EC.visibility_of_element_located(LOGOUT_BUTTON))
-     driver.find_element(By.XPATH, '//*[@id="root"]/div/header/nav/a/p').click()
-     assert  driver.find_element(*INGREDIENT_CARD).is_displayed()
+    def test_go_to_constructor_from_logo(driver, user_data):
+        driver.get(BASE_URL)
+        login(driver, user_data["email"], user_data["password"])
+        WebDriverWait(driver,10).until(EC.visibility_of_element_located(ACCOUNT_HEADER)).click()
+        WebDriverWait(driver,10).until(EC.visibility_of_element_located(LOGOUT_BUTTON))
+        driver.find_element(By.XPATH, '//*[@id="root"]/div/header/nav/a/p').click()
+        assert  driver.find_element(*INGREDIENT_CARD).is_displayed()
 
 #Выход из аккаунта
-def test_logout(driver, user_data):
-     driver.get(BASE_URL)
-     login(driver, user_data["email"], user_data["password"])
-     WebDriverWait(driver, 15).until(EC.visibility_of_element_located(ACCOUNT_HEADER)).click()
-     WebDriverWait(driver, 15).until(EC.visibility_of_element_located(LOGOUT_BUTTON)).click()
-     WebDriverWait(driver, 15).until((EC.visibility_of_element_located(LOGIN_BUTTON)))
-     assert driver.find_element(*LOGIN_BUTTON).is_displayed()
+    def test_logout(driver, user_data):
+        driver.get(BASE_URL)
+        login(driver, user_data["email"], user_data["password"])
+        WebDriverWait(driver, 15).until(EC.visibility_of_element_located(ACCOUNT_HEADER)).click()
+        WebDriverWait(driver, 15).until(EC.visibility_of_element_located(LOGOUT_BUTTON)).click()
+        WebDriverWait(driver, 15).until((EC.visibility_of_element_located(LOGIN_BUTTON)))
+        assert driver.find_element(*LOGIN_BUTTON).is_displayed()
 #Раздел Конструктор
-def test_go_to_buns(driver):
-      driver.get(BASE_URL)
-      WebDriverWait(driver, 10).until(EC.element_to_be_clickable(BUNS_TAB)).click()
-      active_tab = driver.find_element(*BUNS_TAB).get_attribute("class")
-      assert "current" in active_tab
+    def test_go_to_buns(driver):
+        driver.get(BASE_URL)
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(BUNS_TAB)).click()
+        active_tab = driver.find_element(*BUNS_TAB).get_attribute("class")
+        assert "current" in active_tab
 
-def test_go_to_sauces(driver):
-       driver.get(BASE_URL)
-       WebDriverWait(driver, 10).until(EC.element_to_be_clickable(SAUCES_TAB)).click()
-       active_tab = driver.find_element(*SAUCES_TAB).get_attribute("class")
-       assert "current" in active_tab
+    def test_go_to_sauces(driver):
+        driver.get(BASE_URL)
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(SAUCES_TAB)).click()
+        active_tab = driver.find_element(*SAUCES_TAB).get_attribute("class")
+        assert "current" in active_tab
 
-def test_go_to_fillings(driver):
-       driver.get(BASE_URL)
-       WebDriverWait(driver,10).until(EC.element_to_be_clickable(FILLINGS_TAB)).click()
-       active_tab = driver.find_element(*FILLINGS_TAB).get_attribute("class")
-       assert "current" in active_tab
+    def test_go_to_fillings(driver):
+        driver.get(BASE_URL)
+        WebDriverWait(driver,10).until(EC.element_to_be_clickable(FILLINGS_TAB)).click()
+        active_tab = driver.find_element(*FILLINGS_TAB).get_attribute("class")
+        assert "current" in active_tab
 
 
 
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -4,32 +4,33 @@ from selenium.webdriver.support import expected_conditions as EC
 from locators import *
 from config import BASE_URL
 
+class TestRegistration:
 
-def test_registration_success(driver, user_data):
-    driver = webdriver.Chrome()
-    wait = WebDriverWait(driver, 10)
-    driver.get(BASE_URL)
+     def test_registration_success(self, driver, user_data):
+        driver = webdriver.Chrome()
+        wait = WebDriverWait(driver, 10)
+        driver.get(BASE_URL)
 
-    wait.until(EC.visibility_of_element_located(NAME_INPUT)).send_keys(user_data["name"])
-    driver.find_element(*NAME_INPUT).send_keys(user_data["email"])
-    driver.find_element(*PASSWORD_INPUT).send_keys(user_data["password"])
-    driver.find_element(*REGISTER_BUTTON).click()
+        wait.until(EC.visibility_of_element_located(NAME_INPUT)).send_keys(user_data["name"])
+        driver.find_element(*NAME_INPUT).send_keys(user_data["email"])
+        driver.find_element(*PASSWORD_INPUT).send_keys(user_data["password"])
+        driver.find_element(*REGISTER_BUTTON).click()
 
-    account_element = wait.until(EC.visibility_of_element_located(ACCOUNT_HEADER))
-    assert 'Личный Кабинет' in account_element.text
+        account_element = wait.until(EC.visibility_of_element_located(ACCOUNT_HEADER))
+        assert 'Личный Кабинет' in account_element.text
 
 
-def test_registration_short_password(driver, user_data):
-    driver = webdriver.Chrome()
-    wait = WebDriverWait(driver,10)
-    driver.get(BASE_URL)
+     def test_registration_short_password(driver, user_data):
+       driver = webdriver.Chrome()
+       wait = WebDriverWait(driver,10)
+       driver.get(BASE_URL)
 
-    wait.until(EC.visibility_of_element_located(NAME_INPUT)).send_keys(user_data["name"])
-    driver.find_element(*NAME_INPUT).send_keys(user_data["email"])
-    driver.find_element(*PASSWORD_INPUT).send_keys("123")
-    driver.find_element(*REGISTER_BUTTON).click()
+       wait.until(EC.visibility_of_element_located(NAME_INPUT)).send_keys(user_data["name"])
+       driver.find_element(*NAME_INPUT).send_keys(user_data["email"])
+       driver.find_element(*PASSWORD_INPUT).send_keys("123")
+       driver.find_element(*REGISTER_BUTTON).click()
 
-    error_text = wait.until(EC.visibility_of_element_located(REGISTER_ERROR)).text
-    assert "пароль" in error_text.lower()
+       error_text = wait.until(EC.visibility_of_element_located(REGISTER_ERROR)).text
+       assert "пароль" in error_text.lower()
 
 
